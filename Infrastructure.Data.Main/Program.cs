09ac2749c71e1bc959e8976d2630ed9da005cbd6
Singer, Jannik <jsi@axenton.com>
2021-04-28 22:18:21 +0200

    Add project files.

---
+++ Infrastructure.Data.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.Data.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
